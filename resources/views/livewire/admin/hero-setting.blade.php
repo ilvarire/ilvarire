ea784da7182f30c077957faff36c7b56c3bb8a1b
Your Name <you@example.com>
2025-07-03 14:18:49 -0700

    load

--- resources/views/livewire/admin/hero-setting.blade.php
+++ resources/views/livewire/admin/hero-setting.blade.php
@@ -215,7 +215,8 @@
                                 class="w-full px-5 py-3 text-sm font-medium leading-5 text-white text-gray-700 transition-colors duration-150 border border-gray-300 rounded-lg dark:text-gray-400 sm:px-4 sm:py-2 sm:w-auto active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:outline-none focus:shadow-outline-gray">
                                 Cancel
                             </button>
-                            <button type="submit" form="update_hero"
+                            <button type="submit" form="update_hero" wire:loading.class="opacity-50 cursor-not-allowed"
+                                wire:loading.remove.class="active:bg-purple-600 hover:bg-purple-700 focus:shadow-outline-purple"
                                 class="w-full px-5 py-3 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg sm:w-auto sm:px-4 sm:py-2 active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
                                 Save
                             </button>
